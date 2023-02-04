69e4ba18ec0543eb408eafbb4f691274c90f3a45
loannth5 <loannth5@ghtk.co>
2023-02-04 20:48:31 +0700

    add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    title: 'Navigation',
+    initialRoute: '1',
+    routes: {
+      '1': (context) => const FirstScreen(),
+      '2': (context) => const SecondScreen()
+    },
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Scaffold(
+      appBar: AppBar(title: const Text('First Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '2');
+          },
+          child: const Text('On Launcher'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Sencond Screen')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('On Backpress'),
         ),
       ),
     );
