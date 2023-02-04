eaaedcba60052526f53176c7a5c8996246838eff
loannth5 <loannth5@ghtk.co>
2023-02-04 11:25:14 +0700

    init project

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-void main() {s
-  runApp(const MyApp());
+void main() {
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
         ),
-        home: const Text('hello word'),
       ),
     );
   }
 }
-
-class MyAppState extends ChangeNotifier {}
