67e8b33ad04860db8685aeae0b9384964767f1c1
loannth5 <loannth5@ghtk.co>
2023-02-04 20:40:58 +0700

    add demo layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,125 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'Flutter Layout Demo',
+      home: SafeArea(
+          child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Layout'),
         ),
+        body: Center(
+          child: InfomationLocation(),
+        ),
+      )),
+    );
+  }
+}
+
+class InfomationLocation extends StatelessWidget {
+  const InfomationLocation({super.key});
+
+  Column _buildButtonColumn(Color color, IconData iconData, String label) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          iconData,
+          color: color,
+        ),
+        Container(
+          margin: EdgeInsets.only(top: 8),
+          child: Text(label,
+              style: TextStyle(
+                  fontSize: 12, fontWeight: FontWeight.w500, color: color)),
+        )
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'Call'),
+        _buildButtonColumn(color, Icons.route, 'Route'),
+        _buildButtonColumn(color, Icons.share, 'Share'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(8),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
       ),
     );
+    return Column(children: [
+      Image.asset(
+        'images/lake.jpg',
+        width: 600,
+        height: 240,
+        fit: BoxFit.cover,
+      ),
+      TitleSection(),
+      buttonSection,
+      textSection
+    ]);
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(6),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Kandersteg, Switzerland',
+                style: TextStyle(color: Colors.grey[500]),
+              )
+            ],
+          )),
+          Icon(Icons.star),
+          Text('41')
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
