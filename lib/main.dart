2099c143ad3ed704b320136194a0e87a6bfc50fa
loannth5 <loannth5@ghtk.co>
2023-02-05 01:07:00 +0700

    update map and set

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
+import 'dart:collection';
 import 'dart:ffi';
 
 void main() {
-  var numberList = [11, 2, 13, 4];
-  numberList.sort(
-    (a, b) {
-      return a > b ? 1 : 0;
-    },
-  );
-  print("list sort asc: $numberList");
-
-  numberList.sort((a, b) {
-    return a < b ? 1 : 0;
-  });
-  print("list sort dec: $numberList");
+  /**
+   * list
+   */
 
   var productList = <Product>[];
   productList.add(Product('T-Shirt', '1', DateTime.now().millisecond, false));
@@ -22,42 +14,74 @@ void main() {
       .add(Product('Jean', '4', DateTime.now().millisecond + 1000, false));
   productList.add(Product('Jean', '5', DateTime.now().millisecond, true));
   productList.add(Product('Shoe', '6', DateTime.now().millisecond, false));
-  print("list product: ${productList.mapToNameAndDesc()}");
-
-  print(
-      "list product sort alphabetical: ${productList.sortWithNameAcs().mapToNameAndDesc()}");
-
-  print(
-      "list product sort-condition (name+time): ${productList.sortWithNameAndTime().mapToNameAndDesc()}");
+  productList.printProducts('list product:');
+  productList
+      .sortWithNameAcs()
+      .printProducts('list product sort alphabetical:');
+  productList
+      .sortWithNameAndTime()
+      .printProducts('list product sort-condition (name+time):');
 
   print('first product incart=true: ${productList.firstProductInCart().name}');
 
   print('last product incart=true: ${productList.lastProductInCart().name}');
 
-  print(
-      'list product filter incart=true: ${productList.removeWithCodition().mapToNameAndDesc()}');
-
-  var productMap = productList.asMap();
   productList
-      .removeWhere((element) => element.name.contains('a') || element.inCart);
+      .filterProductsInCart()
+      .printProducts('list product filter incart=true:');
 
-  print('list product after remove: ${productList.mapToNameAndDesc()}');
+  productList.removeWithCodition().printProducts('list product after remove:');
 
+/**
+ * set
+ */
   var productSet = productList.toSet();
-  print('set product: ${productSet.mapToNameAndDesc()}');
+  productSet.printProducts('set product:');
   var newProduct = productSet.first.copyTo();
   productSet.add(newProduct);
   productSet.add(productSet.first);
-  print('set product after modifier: ${productSet.mapToNameAndDesc()}');
+  productSet.printProducts('set product after modifier:');
+
+  /**
+   * map
+   */
+
+  var productMap = <int, Product>{};
+  productMap.addAll(productList.asMap().cast<int, Product>());
+  productMap.printKeys('Map keys:');
+  productMap.printValues('Map values');
+  var product = Product('New Dress', '10', DateTime.now().microsecond, true);
+  productMap[1] = product;
+  productMap.printValues('Map insert key=1');
+  productMap[5] = product;
+  productMap.printValues('Map insert key=5');
+  print('containkey = 4: ${productMap.containsKey(4)}');
+  print('containkey = 4: ${productMap.containsKey(5)}');
+  productMap.values
+      .toList()
+      .sortWithNameAcs()
+      .printProducts('Map To List And Sort');
+  productMap.removeWhere((key, value) => value.name.startsWith('T'));
+  productMap.printValues('Map after remove');
+  productMap[1]?.desc = 'edit 10';
+  productMap.printValues('Map after modifer key=1');
+  productMap.putIfAbsent(1, () => product);
+  productMap.printValues('Map after putIfAbsent key=1');
+  productMap.putIfAbsent(-20, () => product);
+  productMap.printValues('Map after putIfAbsent key=10');
+  productMap.printKeys('Map keys:');
+  var productSplayMap = SplayTreeMap();
+  productSplayMap.addAll(productMap);
+  print('Splay keys: ${productSplayMap.keys}');
 }
 
 extension ProductParsing on Iterable<Product> {
-  Iterable<String> mapToNameAndDesc() {
-    return map((e) => '${e.name}-${e.desc}');
+  void printProducts(String prefix) {
+    print('$prefix ${map((e) => '${e.name}-${e.desc}')}');
   }
 }
 
-extension ProductSort on List<Product> {
+extension ProductList on List<Product> {
   List<Product> sortWithNameAcs() {
     sort((a, b) {
       return a.name.compareTo(b.name);
@@ -85,9 +109,23 @@ extension ProductSort on List<Product> {
     return lastWhere((element) => element.inCart);
   }
 
+  Iterable<Product> filterProductsInCart() {
+    return where((element) => element.inCart);
+  }
+
   List<Product> removeWithCodition() {
     removeWhere((element) => element.name.contains('a') || element.inCart);
     return this;
+  }
+}
+
+extension ProductMap on Map<int, Product> {
+  void printValues(String prefix) {
+    values.printProducts(prefix);
+  }
+
+  void printKeys(String prefix) {
+    print('$prefix ${keys}');
   }
 }
 
