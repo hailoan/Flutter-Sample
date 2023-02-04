4c6525a7d0b27b8b919cddc803223a6ca9a63f2f
loannth5 <loannth5@ghtk.co>
2023-02-04 11:17:54 +0700

    init empty project

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {s
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: const Text('hello word'),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {}
